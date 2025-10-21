97458fabd283ce11f6c17d77893eb1fecc175d07
yusun-lab <yu.sun@hyperisland.se>
2025-10-21 22:36:37 +0200

    4.5 is done

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me</button>
 </template>
 
-<script lang="ts">
-// import { Options, Vue } from "vue-class-component";
-export default {
-  name: "App",
-  data() {
-    return {
-      title: "My first Vue 3 App",
-    };
-  },
+<script setup lang="ts">
+// Applicable to vue3.4 and below:
+// import { ref } from "vue";
+// const title = ref("My first Vue 3 app");
+// const name = ref<HTMLInputElement | null>(null);
+
+// Applicable to vue3.5 and above:
+import { useTemplateRef } from "vue";
+
+const title = "My first Vue 3 app";
+const name = useTemplateRef<HTMLInputElement>("name");
+
+const handleClick = () => {
+  console.log(name.value); // directly access the ref object: <input type="text">
+  console.log(name.value?.value); // get the input field's value (optional chaining prevents null errors)
+
+  name.value?.classList.add("active");
+  name.value?.focus();
 };
-// export default class App extends Vue {}
 </script>
 
 <style>
