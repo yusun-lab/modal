338ad3fd8da1e7146cf720d0db4279f68fc063cb
yusun-lab <yu.sun@hyperisland.se>
2025-10-22 11:12:13 +0200

    4.6 is done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="name" />
-  <button @click="handleClick">click me</button>
+  <Modal />
 </template>
 
 <script setup lang="ts">
-// Applicable to vue3.4 and below:
-// import { ref } from "vue";
-// const title = ref("My first Vue 3 app");
-// const name = ref<HTMLInputElement | null>(null);
-
-// Applicable to vue3.5 and above:
+import Modal from "./components/Modal.vue";
 import { useTemplateRef } from "vue";
 
 const title = "My first Vue 3 app";
-const name = useTemplateRef<HTMLInputElement>("name");
-
-const handleClick = () => {
-  console.log(name.value); // directly access the ref object: <input type="text">
-  console.log(name.value?.value); // get the input field's value (optional chaining prevents null errors)
-
-  name.value?.classList.add("active");
-  name.value?.focus();
-};
 </script>
 
 <style>
