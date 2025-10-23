1097d2296a9f7b90ab1acb342f54f3f7183af260
yusun-lab <yu.sun@hyperisland.se>
2025-10-24 00:10:15 +0200

    5.2 is done

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal />
+  <Modal :header="header" :text="text" theme="sale" />
+  <!-- this is how we pass data as props into components which a: makes them more reusable, b: makes them more customizable, and c: allow us to have a single source of truth when it comes to our data -->
 </template>
 
 <script setup lang="ts">
 import Modal from "./components/Modal.vue";
-import { useTemplateRef } from "vue";
+import { ref } from "vue";
 
-const title = "My first Vue 3 app";
+const title = ref("My first Vue 3 app!");
+const header = ref("Sign up for the Giveaway!");
+const text = ref("Grab your ninja swag for half price!");
 </script>
 
 <!-- <style scoped> scoped <style> won't apply its style to anything outside this component-->
 <style>
 #app {
-  /* this app divv right here is not actully inside this component, it's in the public\index.html\<div id="app"></div> */
+  /* this app div right here is not actually inside this component, it's in the public\index.html\<div id="app"></div> */
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="backdrop">
-    <div class="modal">
-      <h1>Modal title</h1>
-      <p>modal content</p>
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
     </div>
   </div>
 </template>
+
+<!-- Vue 2 Options API: -->
+<!-- <script>
+export default {
+  props: ["header", "text"],
+};
+</script> -->
+
+<!-- Vue 3 -->
+<script setup lang="ts">
+const props = defineProps<{
+  header: string | (string | number)[];
+  text: string;
+  theme: string;
+}>();
+</script>
 
 <style>
 /* <style scoped> */
@@ -33,5 +49,14 @@
 
 .modal p {
   font-style: normal;
+}
+
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+
+.modal.sale h1 {
+  color: white;
 }
 </style>
